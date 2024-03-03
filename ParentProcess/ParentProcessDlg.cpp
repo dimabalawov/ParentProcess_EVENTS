d77acef7942495467782f75af00b5a3014a6ea40
dimabalawov <dimabalawov@gmail.com>
2024-03-03 21:33:37 +0200

    Добавьте файлы проекта.

---
+++ ParentProcess/ParentProcessDlg.cpp
@@ -0,0 +1,82 @@
+#include "ParentProcessDlg.h"
+
+ParentProcessDlg* ParentProcessDlg::ptr = NULL;
+
+ParentProcessDlg::ParentProcessDlg(void)
+{
+	ptr = this;
+}
+
+ParentProcessDlg::~ParentProcessDlg(void)
+{
+	
+}
+
+void ParentProcessDlg::Cls_OnClose(HWND hwnd)
+{
+	EndDialog(hwnd, 0);
+}
+
+void MessageAboutError(DWORD dwError)
+{
+	LPVOID lpMsgBuf = NULL;
+	TCHAR szBuf[300]; 
+	BOOL fOK = FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+		NULL, dwError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	if(lpMsgBuf != NULL)
+	{
+		wsprintf(szBuf, TEXT("Ошибка %d: %s"), dwError, lpMsgBuf); 
+		MessageBox(NULL, szBuf, TEXT("Сообщение об ошибке"), MB_OK | MB_ICONSTOP); 
+		LocalFree(lpMsgBuf);
+	}
+}
+
+void ParentProcessDlg::Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    if (IDC_BUTTON1 == id)
+    {
+        HANDLE hEvent = CreateEvent(NULL, TRUE, TRUE, TEXT("{A59FBCB4-154E-49E6-B0C9-0AB3A60942D7}"));
+        if (hEvent == NULL)
+        {
+            MessageAboutError(GetLastError());
+            return;
+        }
+        STARTUPINFO st = { sizeof(st) };
+        PROCESS_INFORMATION pr;
+        TCHAR filename[20];
+        wsprintf(filename, TEXT("%s"), TEXT("CodingThread.exe"));
+        if (!CreateProcess(NULL, filename, NULL, NULL, FALSE, 0, NULL, NULL, &st, &pr))
+        {
+            MessageAboutError(GetLastError());
+            CloseHandle(hEvent);
+            return;
+        }
+        CloseHandle(pr.hThread);
+        CloseHandle(pr.hProcess);
+        ZeroMemory(&st, sizeof(st));
+        st.cb = sizeof(st);
+        wsprintf(filename, TEXT("%s"), TEXT("Standard Dialog.exe"));
+        if (!CreateProcess(NULL, filename, NULL, NULL, FALSE, 0, NULL, NULL, &st, &pr))
+        {
+            MessageAboutError(GetLastError());
+            CloseHandle(hEvent);
+            return;
+        }
+        CloseHandle(pr.hThread);
+        CloseHandle(pr.hProcess);
+
+        CloseHandle(hEvent);
+    }
+}
+
+BOOL CALLBACK ParentProcessDlg::DlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch(message)
+	{
+		HANDLE_MSG(hwnd, WM_CLOSE, ptr->Cls_OnClose);
+		HANDLE_MSG(hwnd, WM_COMMAND, ptr->Cls_OnCommand);
+	}
+	return FALSE;
+}
